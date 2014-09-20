4ee554ffbff746da53007ec2d71fcf2ab3ef9590
function86437 <function86437@gmail.com>
2014-09-20 21:32:23 +0800

    更改滑動效果

--- InnerPage.js
+++ InnerPage.js
@@ -2,55 +2,51 @@
 *   滑鼠包覆時填滿父容器
 *
 */
-divArr = ['Food','Cloth','Live','Trip','Edu','Fun'];
+var divArr = ['Food', 'Cloth', 'Live', 'Trip', 'Edu', 'Fun'];
 var targetID;
 var targetElem;
-window.onload = function(){
-
-    for(var i = 0 ; i < 6 ; i++){
-            if(targetID == divArr[i]){
-
-            }
-    }
-
-
+window.onload = function () {
     document.addEventListener('mousedown',function(){
-                targetID = event.target.id;
-                document.getElementById(targetID).addEventListener("mouseup",function(){
-                    for(var i = 0 ; i < 6 ; i++){
-                        var temp = document.getElementById(divArr[i]);
-
-                        temp.style.width = '81px';
-                    }
-                },false);
-                console.log(targetID);
-                function move(){
-                    var wide = 81;
-                    function frame(){
-                        wide++;
-                        document.getElementById(targetID).style.width = wide + 'px';
-                        if(wide == 460){
-                            window.clearInterval(movement);
-                        }
-                    }
-                    var movement = window.setInterval(frame,5);
+        targetID = event.target.id;
+        for(var i = 0 ; i < 6 ; i++){
+                if(targetID == divArr[i]){
+                    targetElem = document.getElementById(targetID);
                 }
-                move();
-                for(var i = 0 ; i < 6 ; i++){
-                        if(divArr[i] != targetID){
-                            temp = document.getElementById(divArr[i]);
-                            temp.style.width = '5px';
-                        }
+        }
+        function move(){
+            var wide = 81;
+            function frame(){
+                wide++;
+                targetElem.style.width = wide + 'px';
+                if(wide == 460){
+                    window.clearInterval(movement);
                 }
+            }
+            var movement = window.setInterval(frame,3);
+        }
+        move();
 
+        /*
+        *   其他的div內縮
+        */
+        function sharp(temp){
+            var wideSharp = 81;
+            function frameSharp(){
+                wideSharp--;
+                console.log(wideSharp);
+                temp.style.width = wideSharp + 'px';
+                if(wideSharp == 5){
 
-
-
-
+                    window.clearInterval(movementSharp);
+                }
+            }
+            var movementSharp = window.setInterval(frameSharp,5);
+        }
+        for(i = 0 ; i < 6 ; i++){
+            if(divArr[i] != targetID){
+                temp = document.getElementById(divArr[i]);
+                sharp(temp);
+            }
+        }
     },false);
-
-
-
-
-
 }
