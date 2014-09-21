1992a0289ef994ba171741ff420cf3b0b880467d
function86437 <function86437@gmail.com>
2014-09-22 02:27:49 +0800

    index.js modified

---
+++ index.js
@@ -0,0 +1,19 @@
+var secPopup = function(number){
+    var pop = document.getElementById('popup');
+    pop.style.visibility = 'visible';
+    pop.style.top = window.innerHeight/2 - 325 + 'px';
+    pop.style.left = window.innerWidth/2 - 250 + 'px';
+    document.getElementById('pageOverlay').style.visibility = 'visible';
+    var iframe = document.getElementById('iframe');
+    if(number == 2){
+        iframe.setAttribute('src','./InnerPage2.html');
+    } else {
+        iframe.setAttribute('src','./InnerPage.html');
+
+    }
+    pop.appendChild(iframe);
+}
+var closePopup = function(){
+    document.getElementById('popup').style.visibility = 'hidden';
+    document.getElementById('pageOverlay').style.visibility = 'hidden';
+}
